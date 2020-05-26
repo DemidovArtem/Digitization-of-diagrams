7dfc2a541e1f4e197bed35f5c10d0a7570447d8b
Yaroslav Serov <yaroslaw.serow@yandex.ru>
2020-05-26 13:22:39 +0000

    beta.py edited online with Bitbucket

--- RecognApp/beta.py
+++ RecognApp/beta.py
@@ -14,7 +14,6 @@ import random
 import sys
 
 
-<<<<<<< HEAD
 def coordinate_key(person):
     return person.x_coordinate
 
@@ -30,41 +29,21 @@ def x_val_for_column(array_of_column):
             count += 1
             cur = ''
     a[count].x_val = cur
-=======
-def x_val_for_column(columns, text_part):
-    a = sorted(columns, key=lambda col: col.x_coordinate)
-    for j in range(len(a)):
-        a[j].x_val = text_part[j]
->>>>>>> 1c90f030a164d8f78191e055b4575072255e5775
-    return a
 
 
 color_black = (0, 0, 0)
-<<<<<<< HEAD
 #нахождение границ столбиков
 diagram_image = cv2.imread(sys.argv[1])
 boarder = color_recogn.recogn_column(diagram_image, 10)
 #нахождение осей
-=======
-# нахождение границ столбиков
-diagram_image = cv2.imread(sys.argv[1])
-boarder = color_recogn.recogn_column(diagram_image, 10)
-# отрисовка границ столбцов
-array_of_column = color_recogn.draw_boarder(diagram_image, boarder)
-# нахождение осей
->>>>>>> 1c90f030a164d8f78191e055b4575072255e5775
 lines = find_axes.find_axes(sys.argv[1])
 
 axes = {'left': int(lines[0][0][0]),
         'right': int(lines[1][0][0]),
         'bottom': int(lines[2][0][0])}
-<<<<<<< HEAD
+
 #нахождение текста на диаграммах
 bounds, counts, text = find_text.find_text(sys.argv[1], axes, language='English')
-=======
-# нахождение текста на диаграммах
-bounds, counts, text = find_text.find_text(sys.argv[1], axes, array_of_column, language='English')
->>>>>>> 1c90f030a164d8f78191e055b4575072255e5775
 if counts[1] > counts[0]:
     lines.pop(0)
     bounds.pop(0)
@@ -74,8 +53,6 @@ else:
     bounds.pop(1)
     text.pop(1)
 
-
-<<<<<<< HEAD
 #отрисовка
 find_axes.draw_lines_on_image(diagram_image, lines)
 find_text.draw_rectangles(diagram_image, bounds)
@@ -94,20 +71,10 @@ bias = abs(min - lines[1][0][0])
 
 #заполнение значений столбиков
 final_answer_column = x_val_for_column(array_of_column)
-=======
-# отрисовка
-find_axes.draw_lines_on_image(diagram_image, lines)
-find_text.draw_rectangles(diagram_image, bounds)
-
-# заполнение значений столбиков
-final_answer_column = x_val_for_column(array_of_column, text[-1])
->>>>>>> 1c90f030a164d8f78191e055b4575072255e5775
-
 
 parse_text.set_coefficients(bounds[0], text[0])
 
 for i in range(len(final_answer_column)):
-<<<<<<< HEAD
     final_answer_column[i].y_val = parse_text.get_value(final_answer_column[i].y_coordinate, bias)
 
 #печать всех столбиков и их значение в консоль
@@ -122,13 +89,3 @@ for i in final_answer_column:
 
 cv2.imshow('image', diagram_image)
 cv2.waitKey(0)
-=======
-    final_answer_column[i].y_val = parse_text.get_value(final_answer_column[i].y_coordinate)
-
-# печать всех столбиков и их значение в консоль
-for i in range(len(final_answer_column)):
-    print('№', i, '| название', final_answer_column[i].x_val, '| знаечние', final_answer_column[i].y_val, '\n')
-
-cv2.imshow('image', diagram_image)
-cv2.waitKey(0)
->>>>>>> 1c90f030a164d8f78191e055b4575072255e5775
